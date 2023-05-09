ad17b04f3335e8501442ae5f1f8f9d9834c7d508
dmatavel <matavelidanilo@gmail.com>
2023-05-09 15:54:50 -0300

    add some comments

--- tests/test_pwd.c
+++ tests/test_pwd.c
@@ -6,7 +6,7 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 15:22:23 by dmatavel          #+#    #+#             */
-/*   Updated: 2023/05/09 15:52:15 by dmatavel         ###   ########.fr       */
+/*   Updated: 2023/05/09 15:54:40 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
-int	pwd(char *cmd)
+int	pwd(char *cmd) // once we have an function thet eval the cmd's pwd() can be a void arg func
 {
-	if (ft_strncmp(cmd, "pwd", 3) != 0)
+	if (ft_strncmp(cmd, "pwd", 3) != 0) // this if is not necessary in the final code
 		return (1);
 	else if (!getcwd(NULL, 0))
 		return (1);
