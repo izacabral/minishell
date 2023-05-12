9deb86447462b9822b8b4bf5a1650b4faa934408
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2023-05-12 02:24:09 +0000

    add function ft_strncmp

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,7 +1,7 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft.h"
+//# include "libft.h"
 # include "defines.h"
 # include "types.h"
 # include <stdio.h>
--- tests/test_echo.c
+++ tests/test_echo.c
@@ -1,19 +1,39 @@
 #include "../inc/minishell.h"
+/*Realizando alteção no codigo, não esta funcionando*/
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while ((n > 0) && ((s1[i]) || (s2[i])))
+	{
+		if (s1[i] != s2[i])
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+		i++;
+		n--;
+	}
+	return (0);
+}
 
 int echo(char *argv)
 {
-    if(argv=="")
+    if(ft_strncmp(argv[0],"",0))
         printf("\n");
     else
     {
     while(*argv)
         printf("%c",*argv++);
     }
-    printf("\n");
+    if((ft_strncmp(argv[1],"-n",1)==1))
+        printf("\n");
     return 1;
 }
 
 int main()
 {
-    echo("Felipe");
+    char *test;
+    test = "Test";
+    echo(test);
 }
