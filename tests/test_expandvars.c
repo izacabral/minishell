d70b69119040f63c50404003dc70408ac04437fa
Daniel O <96359063+Olin-Dan@users.noreply.github.com>
2023-05-13 03:27:42 -0300

    Add more tests for variable expansion

--- tests/test_expandvars.c
+++ tests/test_expandvars.c
@@ -9,6 +9,8 @@ int	main(void)
 	char	**tab;
 	char	*str1;
 	char	*str2;
+	char	*str3;
+	char	*str4;
 
 	var1 = malloc(sizeof(*var1));
 	var2 = malloc(sizeof(*var2));
@@ -20,14 +22,19 @@ int	main(void)
 	var2->value = NULL;
 	var1->next = var2;
 	var2->next = NULL;
-	tab = malloc(3 * sizeof(*tab));
+	tab = malloc(5 * sizeof(*tab));
 	str1 = ft_strdup("Test variable $a for expansion\n");
 	str2 = ft_strdup("Test $b for no value\n");
+	str3 = ft_strdup("Test variable \"$a some word\" inside quotes\n");
+	str4 = ft_strdup("Test \'$a\' no expansion\n");
 	tab[0] = str1;
 	tab[1] = str2;
-	tab[2] = NULL;
+	tab[2] = str3;
+	tab[3] = str4;
+	tab[4] = NULL;
 	expandvars(tab, var1);
 	ft_putstr_fd(tab[0], 1);
 	ft_putstr_fd(tab[1], 1);
+	ft_putstr_fd(tab[2], 1);
 	return (0);
 }
