85220dbdce012b37d1858c55a8084bd8923435b6
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2023-05-15 19:06:16 -0300

    Update test_exit.c

--- tests/test_exit.c
+++ tests/test_exit.c
@@ -1,15 +1,34 @@
 #include "../inc/minishell.h"
-#include "string.h"
 /*Realizando alteções no codigo*/
 
-int exit(char **arg)
+static int ft_strdigit(char *str)
 {
-    (void)arg;
+    int i;
+    
+    i = 0;
+    if(str == NULL)
+        return (0);
+    while(str[i])
+    {
+        if(ft_isdigit(str[i]) == 0)
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+int ft_exit(t_token	**lst, char **arg)
+{
+    ft_putstr_fd("exit\n",1);
+    if(arg[1] && ft_strdigit(arg[1])==0)
+        ft_putstr_fd("numeric argument required\n",1);
+    clear_token(lst);
     exit(0);	
 }
 
-
 int main(int argc, char **argv)
 { 
-    exit(void);
+    t_token *test;
+    test = malloc(sizeof(t_token));
+    ft_exit(&test,argv);
 }
