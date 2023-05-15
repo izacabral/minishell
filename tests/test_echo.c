a030e8e703e90b9d46bf6be114ef5a82fe853a28
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2023-05-15 16:49:41 -0300

    Update test_echo.c

--- tests/test_echo.c
+++ tests/test_echo.c
@@ -1,54 +1,112 @@
+
 #include "../inc/minishell.h"
 #include "string.h"
 /*Realizando alteções no codigo*/
 
 /*Esta função e para checar os n, se caso na sequencia de n estiver um caracter diferente retorna 1 e vai pular linha*/
-static int check_n(char *str) {
-    int i = 0;
 
-    if (str[0] == '-' && str[1]=='n') {
-        i = 1;
-        while (str[i++] == 'n' && str[i]);
-		if(str[i-1] != 'n')
-			return 0;
+static int	check_n(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '-' && str[1] == 'n')
+	{
+		i = 1;
+		while (str[i] == 'n' && str[i])
+			i++;
+		if (str[i - 1] != 'n')
+			return (0);
 		else
-			return 1;
-    }
-
+			return (1);
+	}
 }
 
-int ft_echo(char **arg)
+int	ft_echo(char **arg)
 {
-	int control;
-	int i;
+	int	control;
+	int	i;
+
 	i = 1;
 	control = 0;
-	if(arg[i] == NULL)
+	if (arg[i] == NULL)
 	{
-		ft_putstr_fd("\n",1);
-		return 0;
-	}
-
-	else if(ft_strncmp(arg[i], "-n", 2) == 0)
-	{
-			control = check_n(arg[i]);
-			if(control == 1)
-				i++;
-	}
-
-	while(arg[i])
-	{
-		ft_putstr_fd(arg[i],1);
-		i++;
-		if(arg[i])
-			 ft_putstr_fd(" ", 1);
-	}
-	if(control == 0)
 		ft_putstr_fd("\n", 1);
-
+		return (0);
+	}
+	else if (ft_strncmp(arg[i], "-n", 2) == 0)
+	{
+		control = check_n (arg[i]);
+		if (control == 1)
+			i++;
+	}
+	while (arg[i++])
+	{
+		ft_putstr_fd (arg[i], 1);
+		if (arg[i])
+			ft_putstr_fd(" ", 1);
+	}
+	if (control == 0)
+		ft_putstr_fd("\n", 1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 { 
-    ft_echo(argv);
+	ft_echo(argv);
+}
+
+#include "../inc/minishell.h"
+#include "string.h"
+/*Realizando alteções no codigo*/
+
+/*Esta função e para checar os n, se caso na sequencia de n estiver um caracter diferente retorna 1 e vai pular linha*/
+
+static int	check_n(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '-' && str[1] == 'n')
+	{
+		i = 1;
+		while (str[i] == 'n' && str[i])
+			i++;
+		if (str[i - 1] != 'n')
+			return (0);
+		else
+			return (1);
+	}
+}
+
+int	ft_echo(char **arg)
+{
+	int	control;
+	int	i;
+
+	i = 1;
+	control = 0;
+	if (arg[i] == NULL)
+	{
+		ft_putstr_fd("\n", 1);
+		return (0);
+	}
+	else if (ft_strncmp(arg[i], "-n", 2) == 0)
+	{
+		control = check_n (arg[i]);
+		if (control == 1)
+			i++;
+	}
+	while (arg[i++])
+	{
+		ft_putstr_fd (arg[i], 1);
+		if (arg[i])
+			ft_putstr_fd(" ", 1);
+	}
+	if (control == 0)
+		ft_putstr_fd("\n", 1);
+}
+
+int	main(int argc, char **argv)
+{ 
+	ft_echo(argv);
 }
