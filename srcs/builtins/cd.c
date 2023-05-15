8d6f1a3b5f7f2b098a2f4d8ecf2f5dc12ffcb384
dmatavel <matavelidanilo@gmail.com>
2023-05-15 16:02:48 -0300

    Tests and drafts for pwd and cd builtins

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -1,22 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/09 13:13:30 by dmatavel          #+#    #+#             */
-/*   Updated: 2023/05/15 15:28:20 by dmatavel         ###   ########.fr       */
+/*   Created: 2023/05/11 13:28:08 by dmatavel          #+#    #+#             */
+/*   Updated: 2023/05/15 16:01:33 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+/*
+ * cd = changes directory
+ * brief: changes from the actual dir to the dir specified in path
+ * and updates the env variables PWD and OLD_PDW
+ * t_env *env: a pointer to a struct containing the PWD and OLD_PWD
+ * vars
+ * char *path: pointer to a string with a designed path
+*/
 
-int	pwd(void)
+int	cd(t_env *env, char *path)
 {
-	if (getcwd(NULL, 0) != NULL)
-		printf("%s\n", getcwd(NULL, 0));
-	else
-		return (1);
+	// PWD = getcwd(NULL, 0);
+	chdir(path);
+	// OLD_PWD = getcwd(NULL, 0);
 	return (0);
 }
