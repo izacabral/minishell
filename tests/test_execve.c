a30184c9f846f4093b8ba9fc95f937f1fc4dfe77
Daniel O <96359063+Olin-Dan@users.noreply.github.com>
2023-05-19 15:48:21 -0300

    Fix: Add comment to test and remove hello_world.c

--- tests/test_execve.c
+++ tests/test_execve.c
@@ -7,6 +7,21 @@ static char	***create_tests1(void);
 static int		test_exec2(char *path);
 static char	***create_tests2(void);
 
+/*
+ * Teste para a função que chama o execve
+ * Compilar com a libft e os arquivos:
+ * tests/test_execve.c srcs/exec/call_execve.c srcs/exec/path_to_lst.c srcs/exec/prefix_slash.c srcs/exec/launch_command.c srcs/data_types/ft_stradd_back.c srcs/data_types/ft_strclear.c srcs/data_types/ft_strdelone.c srcs/data_types/ft_strsetlast.c srcs/data_types/ft_strnew.c srcs/data_types/freetab.c -Itests -Iinc -Ilibft -Llibft -lft
+ *
+ * Criar um arquivo hello_world em separado:
+ *
+ * #include <stdio.h>
+ *
+ * int	main(void)
+ * {
+ *	puts("Hello World!");
+ *	return (0);
+ * }
+ */
 int	main(int argc, char **argv, char **envp)
 {
 	char	*path;
