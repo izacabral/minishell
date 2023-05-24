fbc1f251b32f48340c8898769f6d1112a4485b47
dmatavel <matavelidanilo@gmail.com>
2023-05-24 12:46:30 -0300

    clean pwd.c code and move test_pwd to exclude_test dir/

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:13:30 by dmatavel          #+#    #+#             */
-/*   Updated: 2023/05/22 02:44:03 by dmatavel         ###   ########.fr       */
+/*   Updated: 2023/05/24 12:45:03 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,11 @@
 
 int	pwd(void)
 {
-	char	*abs_path;
+	char	*path;
 
-	abs_path = getcwd(NULL, 0);
-	if (!abs_path)
-	{
-		free(abs_path);
-		abs_path = NULL;
-		return (1);
-	}
-	else
-	{
-		printf("%s\n", abs_path);
-		free(abs_path);
-		abs_path = NULL;
-	}
+	path = getcwd(NULL, 0);
+	printf("%s\n", path);
+	free(path);
+	path = NULL;
 	return (0);
 }
