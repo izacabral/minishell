8961480bce09bea9904871f50daabc462c10a097
Daniel O <96359063+Olin-Dan@users.noreply.github.com>
2023-05-30 18:11:27 -0300

    Exclude unnecessary defines

--- inc/defines.h
+++ inc/defines.h
@@ -6,16 +6,13 @@
 /*   By: izsoares <izsoares@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 20:41:43 by izsoares          #+#    #+#             */
-/*   Updated: 2023/05/14 20:41:48 by izsoares         ###   ########.fr       */
+/*   Updated: 2023/05/30 18:10:11 by daolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINES_H
 # define DEFINES_H
 
-# define BIN	"/bin/"
-# define USRBIN	"/usr/bin/"
-# define LOCBIN	"/usr/local/bin/"
 # define ERROR_BUILTINS -1
 # define SUCESS_BUILTINS 1
 
