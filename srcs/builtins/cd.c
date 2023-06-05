c78dce1e463ebcf921dec1356e285cfb6cb32043
dmatavel <matavelidanilo@gmail.com>
2023-06-05 12:44:43 -0300

    cd now receives a pointer to t_shell struct; test updated

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,47 +6,47 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 15:18:32 by dmatavel          #+#    #+#             */
-/*   Updated: 2023/06/03 14:28:35 by daolivei         ###   ########.fr       */
+/*   Updated: 2023/06/05 12:33:10 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	set_oldpwd(t_shell data)
+void	set_oldpwd(t_shell *data)
 {
 	char	*ptr;
 	char	*oldpwd;
 
 	ptr = getcwd(NULL, 0);
 	oldpwd = ft_strjoin("OLDPWD=", ptr);
-	unset_builtins(1, &oldpwd, &data);
-	export_builtins(1, &oldpwd, &data);
+	unset_builtins(1, &oldpwd, data);
+	export_builtins(1, &oldpwd, data);
 	free(ptr);
 	free(oldpwd);
 }
 
-void	set_pwd(t_shell data)
+void	set_pwd(t_shell *data)
 {
 	char	*ptr;
 	char	*pwd;
 
 	ptr = getcwd(NULL, 0);
 	pwd = ft_strjoin("PWD=", ptr);
-	unset_builtins(1, &pwd, &data);
-	export_builtins(1, &pwd, &data);
+	unset_builtins(1, &pwd, data);
+	export_builtins(1, &pwd, data);
 	free(ptr);
 	free(pwd);
 }
 
-char	*get_home(t_shell data)
+char	*get_home(t_shell *data)
 {
 	t_env	*home;
 
-	home = compare_key(data.lst_env, "HOME");
+	home = compare_key(data->lst_env, "HOME");
 	return (home->value);
 }
 
-int	cd(t_shell data, char *path)
+int	cd(t_shell *data, char *path)
 {
 	int	ret;
 
@@ -73,13 +73,3 @@ int	cd(t_shell data, char *path)
 	return (0);
 }
 
-int	test_cd(t_shell data)
-{
-	int		ret;
-	char	*ptr;
-
-	ptr = NULL;
-	if (cd(data, ptr) == 1)
-		return (ret = 1);
-	return (0);
-}
--- tests/06_test_builtin_cd.c
+++ tests/06_test_builtin_cd.c
@@ -6,80 +6,20 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 09:21:41 by dmatavel          #+#    #+#             */
-/*   Updated: 2023/06/03 14:48:18 by daolivei         ###   ########.fr       */
+/*   Updated: 2023/06/05 12:35:52 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "unit_test.h"
 
-void	set_oldpwd(t_shell data)
-{
-	char	*ptr;
-	char	*oldpwd;
-
-	ptr = getcwd(NULL, 0);
-	oldpwd = ft_strjoin("OLDPWD=", ptr);
-	unset_builtins(1, &oldpwd, &data);
-	export_builtins(1, &oldpwd, &data);
-	free(ptr);
-	free(oldpwd);
-}
-
-void	set_pwd(t_shell data)
-{
-	char	*ptr;
-	char	*pwd;
-
-	ptr = getcwd(NULL, 0);
-	pwd = ft_strjoin("PWD=", ptr);
-	unset_builtins(1, &pwd, &data);
-	export_builtins(1, &pwd, &data);
-	free(ptr);
-	free(pwd);
-}
-
-char	*get_home(t_shell data)
-{
-	t_env	*home;
-
-	home = compare_key(data.lst_env, "HOME");
-	return (home->value);
-}
-
-int	cd(t_shell data, char *path)
-{
-	int	ret;
-
-	set_oldpwd(data);
-	if (!path)
-	{
-		if (!get_home(data))
-		{
-			printf("minishel: cd: HOME not set\n");
-			return (ret = 1);
-		}
-		else
-			chdir(get_home(data));
-	}
-	else
-	{
-		if (chdir(path) == -1)
-		{
-			printf("minishel: cd: %s: No such file or directory", path);
-			return (ret = 1);
-		}
-	}
-	set_pwd(data);
-	return (0);
-}
-
-int	test_cd(t_shell data)
+int	test_cd(t_shell *data)
 {
 	int		ret;
-	char	*ptr;
+	//char	*ptr;
+	char	*path = "42";
 
-	ptr = NULL;
-	if (cd(data, ptr) == 1)
+//	ptr = NULL;
+	if (cd(data, path) == 1)
 		return (ret = 1);
 	return (0);
 }
@@ -91,7 +31,7 @@ int	main(int ac, char **av, char **envp)
 	(void)(ac);
 
 	init_shell(&data, envp);
-	if (test_cd(data) == -1)
+	if (test_cd(&data) == -1)
 		return (1);
 	clear_env(&data.lst_env);
 	return (0);
