b2d434b2e6485831dd6d9fec5a96c2015db1d703
daolivei <daolivei@student.42.fr>
2023-06-06 14:56:47 -0300

    Fix logic check

--- srcs/env/check_varname.c
+++ srcs/env/check_varname.c
@@ -6,7 +6,7 @@
 /*   By: daolivei <daolivei@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 13:29:37 by daolivei          #+#    #+#             */
-/*   Updated: 2023/06/06 13:29:39 by daolivei         ###   ########.fr       */
+/*   Updated: 2023/06/06 14:52:27 by daolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	check_varname(char *key)
 {
-	if (!ft_isalpha(*key) || *key != '_')
+	if (!(ft_isalpha(*key) || *key != '_'))
 		return (1);
 	key++;
 	while(*key)
 	{
-		if (!ft_isalnum(*key) || *key !='_')
+		if (!(ft_isalnum(*key) || *key !='_'))
 			return (1);
 		key++;
 	}
