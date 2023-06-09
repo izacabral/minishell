26ac7cadbd79d09ba08d4bdde7c4d7de7ca473bf
Vanessa <66659404+vanessachabs@users.noreply.github.com>
2023-06-09 00:49:26 +0000

    test_redirect

--- tests/exclude_test/test_redirects.c
+++ tests/exclude_test/test_redirects.c
@@ -1,7 +1,7 @@
 #include "testing.h"
 #include "types.h"
 
-void	error_redir(char *filename)
+void error_redir(char *filename)
 {
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putstr_fd(filename, STDERR_FILENO);
@@ -9,28 +9,31 @@ void	error_redir(char *filename)
 	ft_putendl_fd(strerror(errno), STDERR_FILENO);
 }
 
-int	open_reds(int token, t_sentence cmd, char *file_name) 
-{
-	int fd;
+int open_reds(int token, t_sentence cmd, char *file_name) {
+    int fd;
+    int flags;
 	(void)(cmd);
 
 	if (!file_name)
 		return (EXIT_FAILURE);
-	if (token == OUT) 
-	{
-		fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (token == OUT) {
+        flags = O_WRONLY | O_CREAT | O_TRUNC;
+        fd = open(file_name, flags, 0644);
 		cmd.fd_o = fd;
-	} else if (token == IN) {
-		fd = open(file_name, O_RDONLY);
+    } else if (token == IN) {
+        flags = O_RDONLY;
+        fd = open(file_name, flags);
 		cmd.fd_i = fd;
-	} else if (token == APPEND) 
-	{
-		fd = open(file_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    } else if (token == APPEND) {
+        flags = O_WRONLY | O_CREAT | O_APPEND;
+        fd = open(file_name, flags, 0644);
 		cmd.fd_o = fd;
-	}
-	if (fd == -1) 
-		error_redir(file_name);
-	return fd;
+    }
+    if (fd == -1) {
+        error_redir(file_name);
+        return -1;
+    }
+    return fd;
 }
 
 static char	*find_filename(char *s, int redir)
@@ -41,63 +44,94 @@ static char	*find_filename(char *s, int redir)
 
 	if (redir == IN)
 		str = ft_strchr(s, '<');
-	else if (redir == OUT)
+	else if (redir == OUT || redir == APPEND)
 		str = ft_strchr(s, '>');
-	
 	pos = (str - s) + 2;
 	filename = ft_substr(s, pos , ft_strlen(s) - pos);
+    if (redir == APPEND)
+	{
+        filename = ft_strtrim(filename, " ");
+	}
 	return (filename);
 }
 
-void free_sentence(t_sentence* sentence) {
-	if (sentence == NULL) {
-		return;
-	}
-	if (sentence->args != NULL) {
-		for (int i = 0; sentence->args[i] != NULL; i++) {
-			free(sentence->args[i]);
-		}
-		free(sentence->args);
-	}
-	free(sentence);
-} //trocar por clear_sentence
+int contains_redirection(const char* str, const char* redirection) {
+    if (str == NULL || redirection == NULL)
+        return 0;
+
+    int str_length = ft_strlen(str);
+    int redirection_length = ft_strlen(redirection);
+	int i = 0;
+    if (str_length >= redirection_length) {
+        while( i <= str_length - redirection_length) {
+            int match = 1;
+            for (int j = 0; j < redirection_length; j++) {
+                if (str[i + j] != redirection[j]) {
+                    match = 0;
+                    break;
+                }
+            }
+            if (match) {
+                /*verificar se o caractere seguinte é um espaço em branco
+				assim evita confundir o redirecionamento '>>' com '>' */
+                if (str[i + redirection_length] == ' ') {
+                    return 1;
+                }
+            }
+			i++;
+        }
+    }
+    return 0;
+}
+
 
 int	main(void)
 {
-	t_sentence *st = malloc(sizeof(t_sentence));
-	if (st == NULL) 
-		return -1;
-	char 	*initial_args[] = {"blabla < input.txt", "beaba > output.txt"};
-	int 	num_args = sizeof(initial_args) / sizeof(initial_args[0]);
-	st->args = malloc((num_args + 1) * sizeof(char *));
-	if (st->args == NULL) {
-		free(st);
-		return -1;
-	}
-	for (int i = 0; i < num_args; i++) {
-		st->args[i] = ft_strdup(initial_args[i]);
-		if (st->args[i] == NULL) {
-			free_sentence(st);
-			return -1;
-		}
-	}
-	st->args[num_args] = NULL;
-	st->fd_i = -2;
-	st->fd_o = -2;
-	int	i;
+	char *initial_args[] = { "blabla >> input.txt", "beaba > output.txt"};
+	/*calcula o número de elementos no array initial_args*/
+    int num_args = sizeof(initial_args) / sizeof(initial_args[0]);
+	int i = 0;
 
-	i = -1;
-	while (++i < num_args)
+    t_sentence st;
+    st.args = malloc((num_args + 1) * sizeof(char *));
+    if (st.args == NULL) {
+        printf("Falha na alocação de memória.\n");
+        return 1;
+    }
+	/* percorrendo o array initial_args e fazendo a cópia de cada elemento 
+	para o array st.args da estrutura t_sentence, usando a função strdup.*/
+    while (i < num_args) 
 	{
-		if (strstr(st->args[i], ">") != NULL)
-			open_reds(OUT, *st, find_filename(st->args[i], OUT));
-		else if (strstr(st->args[i], "<") != NULL)
-			open_reds(IN, *st, find_filename(st->args[i], IN));
-		else if (strstr(st->args[i], ">>") != NULL)
-			open_reds(APPEND, *st, find_filename(st->args[i], APPEND));
-		else
-			return (-1);
-	}
-	free_sentence(st);
+        st.args[i] = ft_strdup(initial_args[i]);
+        if (st.args[i] == NULL) {
+            printf("Falha na alocação de memória.\n");
+            for (int j = 0; j < i; j++) {
+                free(st.args[j]);
+            }
+            free(st.args);
+            return 1;
+        }
+		i++;
+    }
+    st.args[num_args] = NULL;
+	i = 0;
+    while (i < num_args) {
+        if (contains_redirection(st.args[i], ">>")) {
+            open_reds(APPEND, st, find_filename(st.args[i], APPEND));
+            printf("Encontrou redirecionamento \">>\": %s\n", st.args[i]);
+        } else if (contains_redirection(st.args[i], ">")) {
+           	open_reds(OUT, st, find_filename(st.args[i], OUT));
+            printf("Encontrou redirecionamento \">\": %s\n", st.args[i]);
+        } else if (contains_redirection(st.args[i], "<")) {
+           	open_reds(IN, st, find_filename(st.args[i], IN));
+            printf("Encontrou redirecionamento \"<\": %s\n", st.args[i]);
+		}
+		i++;
+    }
+	i = 0;
+    while (i < num_args) {
+        free(st.args[i++]);
+    }
+    free(st.args);
 	return 0;
 }
