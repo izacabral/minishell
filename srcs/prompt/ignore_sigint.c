32dfcc567b9a8eac6b095582deddbb802fdb516e
Daniel O <96359063+Olin-Dan@users.noreply.github.com>
2023-06-24 18:39:12 -0300

    Norminette

--- srcs/prompt/ignore_sigint.c
+++ srcs/prompt/ignore_sigint.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ignore_sigint.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daolivei <daolivei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 18:36:24 by daolivei          #+#    #+#             */
+/*   Updated: 2023/06/24 18:38:28 by daolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void    ignore_sigint(void)
+void	ignore_sigint(void)
 {
-    struct sigaction	ign;
+	struct sigaction	ign;
 
 	sigemptyset(&ign.sa_mask);
 	ign.sa_handler = SIG_IGN;
